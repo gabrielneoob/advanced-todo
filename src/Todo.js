3c997697eeb520af389f537d0c840bf197a6dfe5
Neoob <gabrielngc@gmail.com>
2022-11-12 00:33:19 -0300

    deleTodo function

--- src/Todo.js
+++ src/Todo.js
@@ -9,6 +9,7 @@ const Todo = ({ currentTodo, todo, setTodo }) => {
 
   useEffect(() => {
     console.log(currentTodo);
+    console.log(todo);
   }, [todo])
 
   function handleCheck() {
@@ -17,13 +18,20 @@ const Todo = ({ currentTodo, todo, setTodo }) => {
     todo2.completed = !todo2.completed;
     return setTodo(newTodos);
   }
+
+  function deleteTodo() {
+    const newTodos = [...todo];
+    const todos = newTodos.filter((item) => item.id !== currentTodo.id)
+    setTodo(todos);
+  }
+
   return (
     <div className='todo' style={currentTodo.completed ? todoStyle : null}>
       <h3>{currentTodo.content}</h3>
       <div className='todo-btns'>
         <div className='check-btn' onClick={handleCheck}><i className="fa-solid fa-check"></i></div>
         <div className='edit-btn'><i className="fa-solid fa-pencil"></i></div>
-        <div className='delete-btn'><i className="fa-solid fa-x"></i></div>
+        <div className='delete-btn' onClick={deleteTodo}><i className="fa-solid fa-x"></i></div>
       </div>
     </div >
   )
